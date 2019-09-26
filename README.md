2bc73d75b9747c29dc4db62963ea780b24b5152e
Kareem Grant <kareem@getuserwise.com>
2019-09-26 11:04:36 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Homework #3: Iterators
+
+To complete this assignment, do the following:
+
+1. Fork this repo
+2. Clone your forked repo and in terminal, open `pset-iterators.js`, solve the problems.
+3. Ensure your problems work properly!
+4. Open Pull Request (PR) against this repo.
---
+++ pset-iterators.js
@@ -0,0 +1,186 @@
+/*
+Instructions:
+
+For this problem set you will be working with a
+list of U.S. Presidents stored as an array of
+objects. The array has **already been provided for
+you** as a variable named "presidents".
+
+You can see the full list of us presidents in the
+provided file named `us-presidents.js`
+
+All of the following problems require that you
+use the `presidents` array to solve them.
+
+Use any variables or methods deemed necessary
+to solve each problem.
+
+You can use either regular functions or
+arrow functions for your callbacks
+*/
+
+const presidents = require('./us-presidents.js') // <- DO NOT Remove; this represents an array consisting of U.S. Presidents
+
+/***********
+Problem 1:
+
+1. Using the `presidents` array, return the "name" of the
+first president that was a member of the "Whig" party.
+Save the data in a variable called `firstWhigPresident`,
+declared with const
+
+2. Print `firstWhigPresident` to the console
+
+Expected Result Shape: A string representing the full name of the president that meets the criteria
+
+************/
+console.log('Problem 1:')
+
+// Add your code below this line
+
+// Add your code above this line
+
+/** added for formatting purposes **/
+console.log('')
+console.log('-----------------')
+
+/***********
+Problem 2:
+
+1. Using the `presidents` array, return a list of all the
+presidents whose first name was "James".
+Save the data in a variable called `presidentsNamedJames`,
+declared with const
+
+2. Print `presidentsNamedJames` to the console
+
+Expected Result Shape: An array of objects (with each object representing a president)
+
+************/
+console.log('Problem 2:')
+
+// Add your code below this line
+
+// Add your code above this line
+
+/** added for formatting purposes **/
+console.log('')
+console.log('-----------------')
+
+/***********
+Problem 3:
+
+1. Using the `presidents` array, return a list of the
+parties associated each of the presidents.
+Save the data in a variable called `presidentialParties`,
+declared with const
+
+2. Print `presidentialParties` to the console
+
+(partial) Expected Result: ['No Party', 'Federalist', 'Democratic-Republican', 'Democratic-Republican', 'Democratic-Republican', 'Democratic', .... ]
+
+Expected Result Shape: An array of strings representing the party name of each president
+
+Tip: your new array should contain 45 elements
+
+************/
+console.log('Problem 3:')
+
+// Add your code below this line
+
+// Add your code above this line
+
+/** added for formatting purposes **/
+console.log('')
+console.log('-----------------')
+
+/***********
+Problem 4:
+
+1. Using the `presidents` array, return a
+list of the presidents who took office
+between 1850 and 1900. Save the data in a variable called `presidentsBetween1850and1900`, declared with const
+
+2. Print `presidentsBetween1850and1900` to the console
+
+Expected Result Shape: An array of objects (with each object representing a president)
+
+************/
+console.log('Problem 4:')
+
+// Add your code below this line
+
+// Add your code above this line
+
+/** added for formatting purposes **/
+console.log('')
+console.log('-----------------')
+
+/***********
+Problem 5:
+
+1. Using the `presidents` array, return a list of the
+presidents who are still alive today.
+Save the data in a variable called `livingPresidents`,
+declared with const
+
+2. Print `livingPresidents` to the console
+
+Expected Result Shape: An array of objects (with each object representing a president)
+
+************/
+console.log('Problem 5:')
+
+// Add your code below this line
+
+// Add your code above this line
+
+/** added for formatting purposes **/
+console.log('')
+console.log('-----------------')
+
+/***********
+Problem 6:
+
+1. Using the `presidents` array,
+return the "name" of the first "Republican" president.
+Save the data in a variable
+called `firstRepublican`, declared with const
+
+2. Print `firstRepublican` to the console
+
+Expected Result Shape: A string representing the full name of the president that meets the criteria
+
+************/
+console.log('Problem 6:')
+
+// Add your code below this line
+
+// Add your code above this line
+
+/** added for formatting purposes **/
+console.log('')
+console.log('-----------------')
+
+/***********
+Problem 7:
+
+1. Using the `presidents` array, return a
+list of the presidents who served less than 4 years
+in office. Save the data in a variable
+called `shortTermPresidents`, declared with const
+
+2. Print `shortTermPresidents` to the console
+
+Expected Result Shape: An array of objects (with each object representing a president)
+
+************/
+console.log('Problem 7:')
+
+// Add your code below this line
+
+// Add your code above this line
+
+/** added for formatting purposes **/
+console.log('')
+console.log('-----------------')
